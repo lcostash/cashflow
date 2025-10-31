098366699a977f3947ea917590491b7a34d91b11
Lilian Costas <lcostash@gmail.com>
2025-10-31 01:11:25 +0000

    Created the Article module.

---
+++ src/app/Models/Article.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $guarded = ['id'];
+    protected $fillable = ['name'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
